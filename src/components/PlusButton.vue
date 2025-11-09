31d1bcaf7dc4b42a88d9904747b210e6b213fe31
marta <m.nowak033@gmail.com>
2025-11-09 22:16:15 +0100

    Remove unnecessary link wrapper

--- src/components/PlusButton.vue
+++ src/components/PlusButton.vue
@@ -2,9 +2,7 @@
   <v-container>
     <v-row align="center" justify="center">
       <v-col cols="auto">
-        <router-link to="/add-new">
-          <v-btn icon="mdi-plus" size="large"></v-btn>
-        </router-link>
+        <v-btn icon="mdi-plus" size="large" to="/add-new"></v-btn>
       </v-col>
     </v-row>
   </v-container>
