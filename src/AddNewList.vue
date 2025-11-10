6b4fae20881f7c46f217f4ae0fe97d5980b8903d
marta <m.nowak033@gmail.com>
2025-11-10 10:55:49 +0100

    Fetch products list and display it in the combobox

--- src/AddNewList.vue
+++ src/AddNewList.vue
@@ -1,3 +1,41 @@
 <template>
-  <v-input class="border-b-md" />
+  <v-form class="position-absolute w-100 h-100">
+    <v-text-field placeholder="Skriv navn" />
+    <v-combobox :items="productsList" item-value="value" label="Vælg produkt">
+      <!-- Custom slot for how each item appears in the dropdown -->
+      <template v-slot:item="{ props, item }">
+        <v-list-item v-bind="props" title="">
+          <v-list-item-title>{{ item.raw.title }}</v-list-item-title>
+          <v-list-item-subtitle>{{ item.raw.co2_per_kg }} CO₂ per kg</v-list-item-subtitle>
+        </v-list-item>
+      </template>
+    </v-combobox>
+  </v-form>
 </template>
+
+<script>
+import productService from "./services/productService";
+
+export default {
+  data() {
+    return {
+      productsList: [],
+    };
+  },
+  async mounted() {
+    try {
+      const receivedProductsListFromDatabase = await productService.getProductsList();
+      this.productsList = receivedProductsListFromDatabase.map((list) => {
+        return {
+          value: list.prodId,
+          title: list.prodName,
+          co2_per_kg: list.co2_per_kg,
+        };
+      });
+      console.log("products:", this.productsList);
+    } catch (err) {
+      console.error("Error fetching products list", err);
+    }
+  },
+};
+</script>
--- src/services/productService.js
+++ src/services/productService.js
@@ -3,10 +3,21 @@ import { db } from "@/utility/firebaseConfig";
 import { collection, getDocs } from "firebase/firestore";
 
 const shoppingListCollection = collection(db, "indkoebsliste");
+const productsListCollection = collection(db, "Products");
 
 export default {
   async getShoppingList() {
     const snapshot = await getDocs(shoppingListCollection);
+
+    return snapshot.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
+  },
+  async getProductsList() {
+    const snapshot = await getDocs(productsListCollection);
 
     return snapshot.docs.map((doc) => {
       return {
