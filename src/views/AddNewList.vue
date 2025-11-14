208e9d5ac0fd5122bae9ec000751d1b956c4386c
marta <m.nowak033@gmail.com>
2025-11-14 15:05:46 +0100

    Update the "back" button to use the same design as on the listDetails page

--- src/views/AddNewList.vue
+++ src/views/AddNewList.vue
@@ -1,15 +1,21 @@
 <template>
-  <v-form id="list-form" @submit.prevent="handleSaveList">
-    <v-btn size="large" @click="handleClickBackBtn">
-      <v-icon :icon="mdiChevronLeft"></v-icon>Tilbage</v-btn
-    >
+  <v-form id="list-form" fluid @submit.prevent="handleSaveList">
+    <v-row align="center" justify="center">
+      <v-col cols="2" class="pa-0">
+        <v-btn @click="handleClickBackBtn" variant="flat" class="pa-0 ma-0 fill-height fill-width">
+          <v-icon :icon="mdiArrowLeft" size="x-large"></v-icon
+        ></v-btn>
+      </v-col>
 
-    <v-text-field
-      id="name-input"
-      placeholder="Skriv navn"
-      v-model="listName"
-      :rules="validationRules"
-    />
+      <v-col cols="10">
+        <v-text-field
+          id="name-input"
+          placeholder="Skriv navn"
+          v-model="listName"
+          :rules="validationRules"
+        />
+      </v-col>
+    </v-row>
 
     <v-list id="selected-products-list">
       <v-list-item v-for="product in shoppingListProducts" v-bind:key="product">
@@ -44,7 +50,7 @@
 
 <script>
 import productService from "../services/productService";
-import { mdiChevronLeft } from "@mdi/js";
+import { mdiArrowLeft } from "@mdi/js";
 export default {
   data() {
     return {
@@ -53,7 +59,7 @@ export default {
       selectedItem: null,
       listName: "",
       validationRules: [(value) => !!value || "Liste navn er pakrævet."],
-      mdiChevronLeft,
+      mdiArrowLeft,
     };
   },
   methods: {
@@ -99,11 +105,6 @@ export default {
 <style>
 #list-form {
   background-color: white;
-  position: fixed;
-  width: 100vw;
-  height: 100vh;
-  top: 0;
-  left: 0;
   padding: 3vh 5vw;
 }
 
